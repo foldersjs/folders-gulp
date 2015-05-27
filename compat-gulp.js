768a460bd11dfe04225fcae69542eded2c262f9b
Charles Pritchard <chuck@visc.us>
2015-05-26 23:30:50 -0700

    slighty better test wrap on gulp

--- compat-gulp.js
+++ compat-gulp.js
@@ -6,12 +6,21 @@
 
 var path = require('path');
 var Vinyl = require('vinyl');
+var folders = new require('folders');
 
-var cat = {};
+var cat = function() {};
 cat.prototype.to = function(blobStream, base) {
-	var size = blobStream.headers['X-File-Size'];
-	var name = blobStream.headers['X-File-Name'];
+// Headers ain't so nice!
+        var headers = {};
+	var headerMap = blobStream.headers;
+        if(headerMap) for(var i = 0; i < headerMap.length; i++) {
+                var x = headerMap[i].split(':',2);
+                headers[x[0]] = x[1];
+        }
+	var size = headers['X-File-Size'];
+	var name = headers['X-File-Name'];
 	var stream = blobStream.data;
+	if(typeof(stream) == "string") stream = new Buffer(stream);
 	var output = new Vinyl({
 		stat: { size: size },
 		cwd: "/",
@@ -24,8 +33,8 @@ cat.prototype.to = function(blobStream, base) {
 
 cat.prototype.from = function(vinylObj) {
 	var result = vinylObj.stat;
-	var name = path.basename(vinylObj.name);
-	var data = vinlyObj.contents;
+	var name = path.basename(vinylObj.path);
+	var data = vinylObj.contents;
 	var headers = {
 		"Content-Length": result.size,
 		"Content-Type": "application/octet-stream",
@@ -40,18 +49,17 @@ cat.prototype.from = function(vinylObj) {
 	return output;
 };
 
-var ls = {};
-var toLs
+var ls = function (){};
 ls.prototype.to = function(listObj) {
 	var size = listObj.size;
 	var mtime = new Date(parseFloat(listObj.modificationTime));
-	var stream = function() {}; // FIXME: Stream accesses parent server.
+	var stream = Buffer(JSON.stringify(listObj));
 	var base = listObj.uri; // NOTES: May want to clean up this path.
 	var output = new Vinyl({
 		stat: { size: size, mtime: mtime },
 		cwd: "/",
 		base: path.dirname(base),
-		path: uri,
+		path: base,
 		contents: stream,
 	});
 	return output;
@@ -59,8 +67,8 @@ ls.prototype.to = function(listObj) {
 ls.prototype.from = function(vinlyObj) {
 	var modificationTime = vinlyObj.stat.mtime.getTime() + "";
 	var name = path.basename(vinlyObj.path);
-	var size = vinlyObj.size; 
-	var uri = vinlyObj.base;
+	var size = vinlyObj.stat.size; 
+	var uri = vinlyObj.base + "/" + name;
 	var output = {
 		"name": name,
 		"uri": uri,
@@ -73,4 +81,20 @@ ls.prototype.from = function(vinlyObj) {
 	return output;
 };
 
+// "stub-file.txt",
+(new (folders.stub())).cat({data: { streamId: "Test" }, "shareId": "Test" },
+function(data) {
+	var v = new cat();
+	transformed = v.from(v.to(data));
+	console.log("yes", data, transformed);
+});
+
+(new (folders.stub())).ls(".",
+function(data) {
+	var v = new ls();
+	for(var i = 0; i < data.length; i++) {
+		transformed = v.from(v.to(data[i]));
+		console.log("yes", data, transformed);
+	}
+});
 
